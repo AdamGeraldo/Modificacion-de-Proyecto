b029e079e1a1e32a49b651242a2d4d78ef788bc9
Adam Samuel <20198316@itla.edu.do>
2021-10-22 23:19:35 -0400

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase"
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDIciinBbop2bUDm9i4JUqO_Lwn0I66oWI",
-    authDomain: "auth-development-bc93a.firebaseapp.com",
-    projectId: "auth-development-bc93a",
-    storageBucket: "auth-development-bc93a.appspot.com",
-    messagingSenderId: "653381063616",
-    appId: "1:653381063616:web:f23b045c277952dc04ee25"
+  apiKey: "AIzaSyBP_gte_G5Y-Rz5qeSMO3nhB_N9SPTHDFA",
+  authDomain: "admin-ae39d.firebaseapp.com",
+  projectId: "admin-ae39d",
+  storageBucket: "admin-ae39d.appspot.com",
+  messagingSenderId: "701618036916",
+  appId: "1:701618036916:web:fcdaaf60e7bac16c64b7e1"
   };
   
 const app =firebase.initializeApp(firebaseConfig);
